08f4a6c0e433cb13d993ffd20d0d676172f32e4b
sebas <sebas@Saeb>
2025-05-21 09:32:13 -0500

    HOla

---
+++ src/main/java/com/mycompany/sistema_gestion_reserva_transporte/Sistema_Gestion_Reserva_Transporte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_gestion_reserva_transporte;
+
+/**
+ *
+ * @author sebas
+ */
+public class Sistema_Gestion_Reserva_Transporte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
